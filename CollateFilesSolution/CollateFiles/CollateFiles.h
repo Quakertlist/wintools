c5907e0ed92fd4d6c3351e6df06baac7b656d23c
sanghg <houguisang@vip.qq.com>
2022-10-06 01:11:09 +0800

    add: 新增文件过滤移动的工程

---
+++ CollateFilesSolution/CollateFiles/CollateFiles.h
@@ -0,0 +1,32 @@
+
+// CollateFiles.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CCollateFilesApp: 
+// 有关此类的实现，请参阅 CollateFiles.cpp
+//
+
+class CCollateFilesApp : public CWinApp
+{
+public:
+	CCollateFilesApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCollateFilesApp theApp;
